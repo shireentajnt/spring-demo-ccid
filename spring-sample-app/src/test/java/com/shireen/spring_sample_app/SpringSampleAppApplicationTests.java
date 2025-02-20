b67888a224904b3749fee066c3426a2337f22db0
shireennt <shireensonu88@gmail.com>
2025-02-20 09:35:40 +0530

    initial commit

---
+++ spring-sample-app/src/test/java/com/shireen/spring_sample_app/SpringSampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shireen.spring_sample_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSampleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
